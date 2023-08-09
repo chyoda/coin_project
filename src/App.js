64b35eabcbe8cd06fcb9f2ad2f6f3a497af07ce4
ritta <ritavatteroni55555@gmail.com>
2023-08-09 14:40:04 -0300

    modify app.js ritta

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './styles/App.css'
 import Login from './pages/Login';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import HomeEmp from './pages/HomeEmp';
+import HomeCom from './pages/HomeCom';
 
 function App() {
   return (
     <div className="App">
-      <HomeEmp/>
+      <HomeCom/>
     </div>
   );
 }
