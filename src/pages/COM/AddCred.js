7613c627e696a8d4cf649d35f38ec4b3475396fd
a5ur4 <pedroarthurbastosmoreira2999@gmail.com>
2023-08-10 16:56:17 -0300

    update AddCred

--- src/pages/COM/AddCred.js
+++ src/pages/COM/AddCred.js
@@ -1,5 +1,8 @@
 import React from "react";
 import NavBarCOM from "./componentsCOM/NavBarCOM";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import './stylesCOM/AddCred.css';
 
 const AddCred = () => {
     return (
@@ -10,7 +13,22 @@ const AddCred = () => {
                 <br/>
                 <h2>Adicionar Crédito</h2>
                 <br/>
-                
+                <Form>
+                    <Form.Group className="mb-3">
+                        <Form.Label>Nome do cliente:*</Form.Label>
+                        <Form.Control placeholder="Disabled input" disabled />
+                    </Form.Group>
+                    <div>
+                        <Form.Control type="email" placeholder="Ex: 20,00" />
+                    </div>
+                    <Form.Group className="mb-3" controlId="formBasicPassword">
+                        <Form.Label>Senha:*</Form.Label>
+                        <Form.Control type="password" placeholder="Digite a senha da comissão." />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Concluir
+                    </Button>
+                </Form>
             </div>
         </>
     )
