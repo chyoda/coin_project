9b4d090231f106b5161ddf357fe835cd8b993e0c
Anderson Kauan <anderson.kfgoveia@gmail.com>
2023-08-10 14:45:40 -0300

    Atualizacao 1.1

---
+++ library/verificador.php
@@ -0,0 +1,48 @@
+<?php
+
+require_once 'conector.php';
+
+function verificarExistencia($tabelaOrigem, $idAlvo)
+{
+    global $conexao;
+
+    $escapedLogin = mysqli_real_escape_string($conexao, $idAlvo);
+    $quantidade = mysqli_query($conexao, "SELECT * FROM `$tabelaOrigem` WHERE `id` = $escapedLogin");
+
+    if ($quantidade->num_rows >= 0) {
+        return 1;
+    } else {
+        header('HTTP/1.1 404 NOT FOUND');  
+        return 0;
+    }
+}
+
+function verificarExistenciaEmpresa($nomeEmpresa)
+{
+    global $conexao;
+
+    $quantiEmpresa = mysqli_query($conexao, "SELECT `id` FROM `enterprise` WHERE `name` = $nomeEmpresa");
+
+    if ($quantiEmpresa->num_rows == 1) {
+        return 1;
+    } else {
+        header('HTTP/1.1 404 NOT FOUND');  
+        echo "Empresa não encontrada";
+        return 0;
+    }
+}
+
+function Autenticador($tabelaOrigem, $login)
+{
+    global $conexao;
+
+    $escapedLogin = mysqli_real_escape_string($conexao, $login);
+    $quantidade = mysqli_query($conexao, "SELECT * FROM `$tabelaOrigem` WHERE `id` = $escapedLogin");
+
+    if ($quantidade->num_rows >= 0) {
+        return 1;
+    } else {
+        header('HTTP/1.1 404 NOT FOUND');  
+        return 0;
+    }
+}
