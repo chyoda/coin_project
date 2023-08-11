5f5801532d163729425fe339565c661959e00bd1
Anderson Kauan <anderson.kfgoveia@gmail.com>
2023-08-11 11:28:27 -0300

    Versão 2.0

--- backend/cadastrarCliente.php
+++ backend/cadastrarCliente.php
@@ -3,9 +3,25 @@
 require_once '..\library\conector.php';
 require_once '..\api_php\verificador.php';
 
-$nomeCliente = $_POST['nomeCliente'];
-$dataNascimento = $_POST['dataCliente'];
+global $conexao;
 
-$dataIntermediaria = strtotime($dataDoFrontend);
+function gerarGuid()
+{
+    $guid = uniqid("TheWeeknd", true);
+    return $guid;
+}
+
+$guid = gerarGuid();
+
+// $nomeCliente = $_POST['nomeCliente'];
+// $dataNascimento = $_POST['dataCliente'];
+
+$nomeCliente = 'Aw21 Jorge';
+$dataNascimento = '5-02-2006';
+
+$dataIntermediaria = strtotime($dataNascimento);
 $dataFormatadaParaDB = date('Y-m-d', $dataIntermediaria);
 
+mysqli_query($conexao, "INSERT INTO `client` (`guid`, `name`, `birth_date`) VALUES ('$guid', '$nomeCliente', '$dataFormatadaParaDB')");
+header('HTTP/1.1 201 CREATED');
+exit();
