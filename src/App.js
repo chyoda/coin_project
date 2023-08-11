b79fce53fc5cc5910f96b57b03488f8c5deb104d
a5ur4 <pedroarthurbastosmoreira2999@gmail.com>
2023-08-11 11:38:18 -0300

    Add footer on app.js and start MonEmp

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom'; 
 import AppRoutesCom from './AppRoutesCom';
-import Login from './pages/Login.js'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-        {/* <AppRoutesCom/> */}
-        <Login/>
+        <AppRoutesCom/>
+        <Footer/>
       </div>
     </BrowserRouter>
   );
