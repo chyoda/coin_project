502d8d693322f5d0b9ddaf25e5df91363741ead1
Anderson Kauan <anderson.kfgoveia@gmail.com>
2023-08-28 10:04:22 -0300

    Evento de cadastro concluido.

--- src/pages/COM/AddEmp.js
+++ src/pages/COM/AddEmp.js
@@ -2,29 +2,53 @@ import React from "react";
 import NavBarCOM from "./componentsCOM/NavBarCOM";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';    
-import './StyleCOM.css'
+import './StyleCOM.css';
+import $ from 'jquery';  // Importar a biblioteca jQuery
+
 
 const AddEmp = () => {
+    
+    const handleSubmit = (event) => {
+
+        console.log("O código foi executado até aqui.")
+        event.preventDefault(); // Impedir o envio padrão do formulário
+
+        const formData = {
+            nomeEmpresa: event.target.nomeEmpresa.value,
+            descricao: event.target.descricao.value,
+            saldo: event.target.saldo.value
+        };
+
+        $.ajax({
+            url: "/backend/cadastrarEmpresa.php", // Caminho relativo à raiz do projeto
+            method: "POST",
+            data: formData,
+            success: function (response) {
+                console.log("Sucesso:", response);
+                // Redirecionar ou atualizar a página, se necessário
+            },
+            error: function (error) {
+                console.error("Erro ao enviar dados:", error);
+            }
+        });
+    };
+
     return (
         <>
             <NavBarCOM/>
             <br/>
-            <Form className='boxCred'>
+            <Form className='boxCred' onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicText">
                     <Form.Label>Nome da empresa:*</Form.Label>
-                    <Form.Control type="text" maxLength={30} placeholder="Digite o nome do cliente." />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicText">
-                    <Form.Label>Representante:*</Form.Label>
-                    <Form.Control type="text" maxLength={30} placeholder="Digite o representante da empresa." />
+                    <Form.Control name='nomeEmpresa' type="text" maxLength={30} placeholder="Digite o nome da empresa." />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicText">
                     <Form.Label>Descrição:*</Form.Label>
-                    <Form.Control type="text" maxLength={70} placeholder="Digite a descrição da empresa." />
+                    <Form.Control name='descricao' type="text" maxLength={70} placeholder="Digite a descrição da empresa." />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicNumber">
                     <Form.Label>Saldo*:</Form.Label>
-                    <Form.Control type="number" max={9999.99} step="0.01" placeholder="Digite o saldo da empresa." />
+                    <Form.Control name='saldo' type="number" max={99999999.99} step="0.01" placeholder="Digite o saldo da empresa." />
                 </Form.Group>
                 <br/>
                 <Button variant="primary" type="submit">
@@ -35,4 +59,4 @@ const AddEmp = () => {
     )
 }
 
-export default AddEmp
+export default AddEmp;
--- src/pages/COM/CadCliente.js
+++ src/pages/COM/CadCliente.js
@@ -1,29 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import EmptyNavBar from "../../components/EmptyNavBar";
-import '../../styles/CadCliente.css'
+import '../../styles/CadCliente.css';
 import CloseButton from 'react-bootstrap/CloseButton';
+<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>;
 
 
-const CadCliente = () =>{
-    return(
+const CadCliente = () => {
+
+    // É necessário iniciar um servidor PHP: php -S localhost:3000
+
+    const [clienteData, setClienteData] = useState({
+        nome: "",
+        dataNascimento: ""
+    });
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setClienteData((prevData) => ({
+            ...prevData,
+            [name]: value
+        }));
+    };
+
+    const handleSubmit = () => {
+        // Aqui você pode enviar os dados para o servidor usando fetch ou outra biblioteca.
+    };
+
+    return (
         <div>
             <EmptyNavBar />
             <CloseButton />
-            <h1 class='text'>Cadastrar Cliente</h1>
-            <div class='card'>
-                <label class='letra'>Nome:</label>
-                <input placeholder="Digite o nome do cliente" class='input'></input>
-                <br></br>
-                <label class='letra'>Data de nascimento:</label>
-                <input placeholder="Digite a data de nascimento do cliente" class='input'></input>
-                <br></br>
-                <button type="button" class="btn">Concluir</button>
+            <h1 className='text'>Cadastrar Cliente</h1>
+            <div className='card'>
+                <label className='letra'>Nome:</label>
+                <input
+                    name="nome"
+                    placeholder="Digite o nome do cliente"
+                    className='input'
+                    onChange={handleChange}
+                    value={clienteData.nome}
+                />
+                <br />
+                <label className='letra'>Data de nascimento:</label>
+                <input
+                    name="dataNascimento"
+                    placeholder="Digite a data de nascimento do cliente"
+                    className='input'
+                    onChange={handleChange}
+                    value={clienteData.dataNascimento}
+                />
+                <br />
+                <button type="button" className="btn" onClick={handleSubmit}>
+                    Concluir
+                </button>
             </div>
         </div>
-    )
-}
-
-
-
+    );
+};
 
 export default CadCliente;
